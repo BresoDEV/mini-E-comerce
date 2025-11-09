7675c5c2386ea40f0ef78cd562a7ebccc10d2843
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-11-09 18:58:56 -0300

    a

---
+++ itens.js
@@ -0,0 +1,21 @@
+const PRODUCTS = [
+    {
+        id: 1,
+        nome: 'Camisa Dark',
+        preco: 89.90,
+        imagem: 'https://m.media-amazon.com/images/I/21M-OVdFoiL._AC_.jpg',
+        desc:'blabla blabla'
+    },
+    {
+        id: 2, 
+        nome: 'Jaqueta Sombria', 
+        preco: 199.90, 
+        imagem: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/a40c88223b16411fcbccbb169336bde9.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp'
+    },
+    {
+        id: 3,
+        nome: 'Boné Noturno',
+        preco: 59.90,
+        imagem: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTu2AXbChCvBS0UBgz2nYsfBLGYlOuFLR6oQEk8hrKbESiK_BqIDgJLonv71aVfrnChNuCe_PBa0BkhEeSTWj7h30eJbY1gQgxgLnw5qEqXM8ZhRXNQFCORhG-X26t0J3Fwir_Qzw&usqp=CAc'
+    }
+];
