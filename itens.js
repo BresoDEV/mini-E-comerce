1e259fd12b494e0ff703ca2029a2c344c6530b36
BresoDEV <75597388+BresoDEV@users.noreply.github.com>
2025-11-09 19:00:25 -0300

    Commit automatico em 09/11/2025 ├ás 19:00:25,53

--- itens.js
+++ itens.js
@@ -10,12 +10,14 @@ const PRODUCTS = [
         id: 2, 
         nome: 'Jaqueta Sombria', 
         preco: 199.90, 
-        imagem: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/a40c88223b16411fcbccbb169336bde9.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp'
+        imagem: 'https://img.kwcdn.com/product/Fancyalgo/VirtualModelMatting/a40c88223b16411fcbccbb169336bde9.jpg?imageMogr2/auto-orient%7CimageView2/2/w/800/q/70/format/webp',
+        desc:'blabla blabla'
     },
     {
         id: 3,
         nome: 'Boné Noturno',
         preco: 59.90,
-        imagem: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTu2AXbChCvBS0UBgz2nYsfBLGYlOuFLR6oQEk8hrKbESiK_BqIDgJLonv71aVfrnChNuCe_PBa0BkhEeSTWj7h30eJbY1gQgxgLnw5qEqXM8ZhRXNQFCORhG-X26t0J3Fwir_Qzw&usqp=CAc'
+        imagem: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcTu2AXbChCvBS0UBgz2nYsfBLGYlOuFLR6oQEk8hrKbESiK_BqIDgJLonv71aVfrnChNuCe_PBa0BkhEeSTWj7h30eJbY1gQgxgLnw5qEqXM8ZhRXNQFCORhG-X26t0J3Fwir_Qzw&usqp=CAc',
+        desc:'blabla blabla'
     }
 ];
